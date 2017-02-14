1b9cc904d806e3f496f7c4331a6c6df13712cbf2
eugeniusme <zhevedmid@rambler.ru>
2017-02-15 00:12:23 +0300

    Update recommender_films.py

--- recommender_films.py
+++ recommender_films.py
@@ -1,49 +1,60 @@
 import json
 import codecs
 
-with codecs.open('google.json', "r", encoding='utf-8') as f:
-    movies = json.load(f)
+def existance_of_title(movies, myMovie):
+    for movie in movies:
+        if movie['title'] == myMovie:
+            return movie
+    return {}
 
-print('Введите название фильма:')
-myMovie = input()
-check=0
-for movie in movies:
-    if movie['title'] == myMovie:
-        check = 1
-        inputMovie = movie
-        break
-if check == 0:
-    print('Такого фильма в реестре нет. Программа завершилась.')
-    exit(0)
-genres = [film['name'] for film in inputMovie['genres']]
-collection = []
-recommend = {}
 
-for film in movies:
-    if inputMovie['adult'] == film['adult']:
-        genresFilm = [film['name'] for film in inputMovie['genres']]
-        recommend[film['title']] = len(set(genres)&set(genresFilm)) * 10 + (10 - abs(film['vote_average'] - inputMovie['vote_average']))
+def get_recommend(movies, inputMovie):
+    recommend = {}
+    genres = [film['name'] for film in inputMovie['genres']]
+    for film in movies:
+        if inputMovie['adult'] == film['adult']:
+            genresFilm = [film['name'] for film in inputMovie['genres']]
+            recommend[film['title']] = len(set(genres)&set(genresFilm)) * 10 + (10 - abs(film['vote_average'] - inputMovie['vote_average']))
+    return recommend
 
-    try:
-        collectFilm = film['belongs_to_collection']['name']
-        collectInputMovie = inputMovie['belongs_to_collection']['name']
-    except TypeError:
-        continue
+
+def get_collection(movies, inputMovie):
+    collection = []
+    for film in movies:
+        try:
+            collectFilm = film['belongs_to_collection']['name']
+            collectInputMovie = inputMovie['belongs_to_collection']['name']
+        except TypeError:
+            continue
+        else:
+            if collectFilm == collectInputMovie and inputMovie['title'] != film['title']:
+                collection.append(film['title'])
+    return collection
+
+
+def print_recommend(collection, recommend, inputMovie):
+    if len(collection) < 20:
+        iter = len(collection)
+        for keys, values in sorted(recommend.items(), key=lambda x: x[1], reverse=True):
+            if keys not in collection and keys != inputMovie['title']:
+                iter += 1
+                collection.append(keys)
+            if iter == 20:
+                return collection
     else:
-        if collectFilm == collectInputMovie and inputMovie['title'] != film['title']:
-            collection.append(film['title'])
+        return collection
 
-print('Рекомендованные фильмы:')
-if len(collection) < 20:
-    [print(i) for i in collection]
-    iter = len(collection)
+if __name__ == '__main__':
+    with codecs.open('google.json', "r", encoding='utf-8') as f:
+        movies = json.load(f)
 
-    l = lambda x: x[1]
-    for keys, values in sorted(recommend.items(), key=l, reverse=True):
-        if keys not in collection and keys != inputMovie['title']:
-            iter += 1
-            print(keys)
-        if iter == 20:
-            break
-else:
-    [print(collection[i]) for i in range(10)]
+    myMovie = input('Введите название фильма: ')
+    inputMovie = existance_of_title(movies, myMovie)
+    if inputMovie == {}:
+        print('Такого фильма в реестре нет. Программа завершилась.')
+    else:
+        genres = [film['name'] for film in inputMovie['genres']]
+        collection = get_collection(movies, inputMovie)
+        recommend = get_recommend(movies, inputMovie)
+        print('Рекомендованные фильмы:')
+        [print(i) for i in print_recommend(collection, recommend, inputMovie)]
