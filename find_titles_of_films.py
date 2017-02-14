8d19d3409d2921f3f9da7428ae43e740bdf9412f
eugeniusme <zhevedmid@rambler.ru>
2017-02-15 00:11:56 +0300

    Update find_titles_of_films.py

--- find_titles_of_films.py
+++ find_titles_of_films.py
@@ -4,6 +4,5 @@ import codecs
 if __name__ == '__main__':
     with codecs.open('google.json', "r", encoding='utf-8') as f:
         movies = json.load(f)
-
     sub = input('Введите искомую подстроку: ')
     [print(movie['title']) for movie in movies if sub.lower() in movie['title'].lower()]
