884d2e8fc63207ab1902f789877145f506676ee7
eugeniusme <zhevedmid@rambler.ru>
2017-02-15 00:10:41 +0300

    Update budget_of_saw_2.py

--- budget_of_saw_2.py
+++ budget_of_saw_2.py
@@ -12,12 +12,12 @@ def load_json_data_from_url(base_url, url_params):
 def make_tmdb_api_request(method, api_key, extra_params=None):
     extra_params = extra_params or {}
     url = 'https://api.themoviedb.org/3%s' % method
-    params = {
-        'api_key': api_key,
-        'language': 'ru',
-    }
+    params = {'api_key': api_key,
+        'language': 'ru',}
     params.update(extra_params)
     return load_json_data_from_url(url, params)
 
-
-print('Budget of Saw II:', (make_tmdb_api_request(method='/movie/' + str(215), api_key='***')['budget']))
+if __name__ == '__main__':
+    id_of_film = 215
+    api = '319d54c2728dc4563bde29d5c4466336'
+    print('Budget of Saw II:', (make_tmdb_api_request(method='/movie/' + str(id_of_film), api_key=api)['budget']))
