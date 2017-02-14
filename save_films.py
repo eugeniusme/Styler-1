dc99f8f5706b06dec0eefe17274948ca4904f30e
eugeniusme <zhevedmid@rambler.ru>
2017-02-15 00:11:11 +0300

    Update save_films.py

--- save_films.py
+++ save_films.py
@@ -2,33 +2,18 @@ import urllib.request
 import urllib.parse
 import json
 
-
-def load_json_data_from_url(base_url, url_params):
-    url = '%s?%s' % (base_url, urllib.parse.urlencode(url_params))
-    response = urllib.request.urlopen(url).read().decode('UTF-8')
-    return json.loads(response)
-
-
-def make_tmdb_api_request(method, api_key, extra_params=None):
-    extra_params = extra_params or {}
-    url = 'https://api.themoviedb.org/3%s' % method
-    params = {
-        'api_key': api_key,
-        'language': 'ru',
-    }
-    params.update(extra_params)
-    return load_json_data_from_url(url, params)
-
-
-f = open('google.json', 'w', encoding='utf-8')
-movies = []
-i = 1
-while i < 1001:
-    try:
-        righ = dict(make_tmdb_api_request(method='/movie/' + str(i), api_key='***'))
-    except Exception:
-        i += 1
-    else:
-        movies.append(righ)
-        i += 1
-json.dump(movies, f, ensure_ascii=False)
+if __name__ == '__main__':
+    api = '319d54c2728dc4563bde29d5c4466336'
+    with open('google.json', 'w', encoding='utf-8') as f:
+        movies = []
+        id_of_film = 1
+        while id_of_film < 1001:
+            url = 'https://api.themoviedb.org/3/movie/' + str(id_of_film)  + '?api_key=' + api + '&language=ru'
+            try:
+                data_of_film = json.loads(urllib.request.urlopen(url).read().decode('UTF-8'))
+            except Exception:
+                id_of_film += 1
+            else:
+                movies.append(data_of_film)
+                id_of_film += 1
+        json.dump(movies, f, ensure_ascii=False)
