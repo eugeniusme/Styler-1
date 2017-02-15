a915e8b623fa72d637c8c41843a565c89ec33f7e
eugeniusme <zhevedmid@rambler.ru>
2017-02-16 00:27:16 +0300

    Update budget_of_saw_2.py

--- budget_of_saw_2.py
+++ budget_of_saw_2.py
@@ -2,22 +2,11 @@ import urllib.request
 import urllib.parse
 import json
 
-
-def load_json_data_from_url(base_url, url_params):
-    url = '%s?%s' % (base_url, urllib.parse.urlencode(url_params))
-    response = urllib.request.urlopen(url).read().decode('utf-8')
-    return json.loads(response)
-
-
-def make_tmdb_api_request(method, api_key, extra_params=None):
-    extra_params = extra_params or {}
-    url = 'https://api.themoviedb.org/3%s' % method
-    params = {'api_key': api_key,
-        'language': 'ru',}
-    params.update(extra_params)
-    return load_json_data_from_url(url, params)
+def load_from_tmbd(id_of_film, api):
+    url = 'https://api.themoviedb.org/3/movie/' + str(id_of_film)  + '?api_key=' + api + '&language=ru'
+    return json.loads(urllib.request.urlopen(url).read().decode('UTF-8'))
 
 if __name__ == '__main__':
     id_of_film = 215
     api = input('Please input key') 
-    print('Budget of Saw II:', (make_tmdb_api_request(method='/movie/' + str(id_of_film), api_key=api)['budget']))
+    print('Budget of Saw II:', (load_from_tmbd(method='/movie/' + str(id_of_film), api_key=api)['budget']))
